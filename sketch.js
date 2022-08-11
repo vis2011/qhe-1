5542db9c620a763b15a3db498c9e82245e1cdc7e
vis2011 <75988290+vis2011@users.noreply.github.com>
2022-08-11 20:11:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+
+var play,how
+
+var about=document.getElementsById("bt")
+
+
+function preload(){
+
+}
+
+
+function setup(){
+
+    createCanvas(windowWidth,windowHeight)
+
+}
+
+
+function draw(){
+
+
+about.mousePressed(() => {
+      console.log("working!");
+    })
+
+
+
+
+drawSprites()
+}
